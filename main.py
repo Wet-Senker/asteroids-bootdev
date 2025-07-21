a9b21ea68f679374635aa5bdbc816eb219221a05
Wet-Senker <dhrnickdevries@hotmail.com>
2025-07-21 17:46:48 +0200

    sets fps to 60

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
-    while running:
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))        
         pygame.display.flip()
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
