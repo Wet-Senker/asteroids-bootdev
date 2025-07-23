6348707b451e22de65cff365d508902a73596384
Wet-Senker <dhrnickdevries@hotmail.com>
2025-07-23 14:58:18 +0200

    Ship shoots bullets

--- src/player.py
+++ src/player.py
@@ -43,5 +43,5 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
-        shot = Shot(*self.position)
-        shot.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
